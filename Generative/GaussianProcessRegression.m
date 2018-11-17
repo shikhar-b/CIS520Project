beaa4ab1d65e13fe2af0babfc0aa2da89f531627
Vatsal <vatsal.chanana@gmail.com>
2018-11-17 16:42:09 -0500

    Restructuring

--- Generative/GaussianProcessRegression.m
+++ Generative/GaussianProcessRegression.m
@@ -1,4 +1,4 @@
-function test_labels = GaPrRe(train_inputs, train_labels, test_inputs)
+function test_labels = GaussianProcessRegression(train_inputs, train_labels, test_inputs)
     numoutputs = size(train_labels,2);
     test_labels = zeros(size(test_inputs,1),numoutputs);
     for i=1:numoutputs

bebde0a007025751b37d4c0d2c5e086accc5c9ea
Vatsal <vatsal.chanana@gmail.com>
2018-11-17 17:08:13 -0500

    GLM on data

--- predict_labels.m
+++ predict_labels.m
@@ -2,8 +2,8 @@ function pred_labels=predict_labels(train_inputs,train_labels,test_inputs)
 
 train_inputs = preprocess(train_inputs);
 test_inputs  = preprocess(test_inputs);
-pred_labels = GaussianProcessRegression(train_inputs, train_labels, test_inputs);
+%pred_labels = GaussianProcessRegression(train_inputs, train_labels, test_inputs);
 %pred_labels=randn(size(test_inputs,1),size(train_labels,2));
-
+pred_labels = glm(train_inputs, train_labels, test_inputs);
 end
 

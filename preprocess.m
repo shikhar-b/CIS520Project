e8388a740008b89587ade70a79ff123734e19dd8
Vatsal <vatsal.chanana@gmail.com>
2018-11-22 15:52:23 -0500

    Updated PCA to use the same weights

--- preprocess.m
+++ preprocess.m
@@ -1,7 +1,8 @@
-function data=preprocess(inputs)
+function [train_data, test_data] =preprocess(inputs, test_inputs)
     X_tweets = inputs(:,22:2021);
     X_demo = inputs(:,1:21);
-    [coeff, score, latent, tsquared, explained] = pca(X_tweets);
-    X_tweets_reduced = score(:,1:25);
-    data = [X_demo, X_tweets_reduced];
-    
+    [coeff, score, latepnt, tsquared, explained] = pca(X_tweets);
+    coeff_selected = coeff(:, 1:50);
+    X_tweets_reduced = X_tweets * coeff_selected;
+    train_data = [X_demo, X_tweets_reduced];
+    test_data = [test_inputs(:,1:21), test_inputs(:,22:2021)*coeff_selected];

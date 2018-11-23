03ec1b86ea262bf7fbaab576b6b95ecfd68c0e40
Dewang Sultania <dewangsultania@gmail.com>
2018-11-23 02:36:43 -0500

    Changed Neural Network Parameters

--- Generative/GRNN.m
+++ Generative/GRNN.m
@@ -3,6 +3,6 @@ function test_labels = GRNN(train_inputs, train_labels, test_inputs)
     test_labels = zeros(size(test_inputs,1),numoutputs);
     for i=1:numoutputs
         train_labels_i = train_labels(:,i);
-        ngrnn = newgrnn(train_inputs',train_labels_i');
+        ngrnn = newgrnn(train_inputs',train_labels_i',2.0);
         test_labels(:,i) = sim(ngrnn,test_inputs');
     end

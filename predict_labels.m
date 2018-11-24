e42c514b0942c7a2dd2d0fe4ae2757744db3c05e
Dewang Sultania <dewang@eniac.seas.upenn.edu>
2018-11-24 13:13:30 -0500

    Baseline0 beaten

--- predict_labels.m
+++ predict_labels.m
@@ -7,12 +7,13 @@ addpath('InstanceBased');
 addpath('helper');
 addpath('GLM');
 addpath('glmnet_matlab');
-[train_inputs, test_inputs] = preprocess(train_inputs, test_inputs);
+[train_inputs_pre, test_inputs_pre] = preprocess(train_inputs, test_inputs);
 pred_labels_1 = glm(train_inputs, train_labels, test_inputs);
-pred_labels_2 = RandomForest(train_inputs, train_labels, test_inputs);
+pred_labels_2 = GaussianProcessRegression(train_inputs_pre, train_labels, test_inputs_pre);
+pred_labels_3 = RandomForest(train_inputs_pre, train_labels, test_inputs_pre);
 %pred_labels_3 = NNtest(train_inputs, train_labels, test_inputs);
 %pred_labels_4 = GRNN(train_inputs, train_labels, test_inputs);
-pred_labels = (0.80.*pred_labels_1 + 0.20.*pred_labels_2);
+pred_labels = 0.70.*pred_labels_1+0.20.*pred_labels_2+0.10.*pred_labels_3;
 %pred_labels=randn(size(test_inputs,1),size(train_labels,2));
 %pred_labels = glm(train_inputs, train_labels, test_inputs);
 end

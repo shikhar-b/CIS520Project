1548e41207f81a1b96c761ea20d376568fad9090
DEWANG SULTANIA <dewang@dc1.base.seas.upenn.edu>
2018-12-04 18:12:19 -0500

    Using Preprocess with KMeans

--- preprocess.m
+++ preprocess.m
@@ -6,6 +6,7 @@ function [train_data, test_data] =preprocess(inputs, test_inputs, n_comp)
     X_tweets = inputs(:,22:2021);
     X_demo = inputs(:,1:21);
     [coeff, score, latepnt, tsquared, explained] = pca(X_tweets);
+    n_comp = min(n_comp, size(coeff,2));
     coeff_selected = coeff(:, 1:n_comp);
     X_tweets_reduced = X_tweets * coeff_selected;
     train_data = [X_demo, X_tweets_reduced];

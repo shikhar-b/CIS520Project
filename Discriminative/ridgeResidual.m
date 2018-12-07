50ba24ba32ae80473f98625dbd2cc56ba042696b
Vatsal <vatsal.chanana@gmail.com>
2018-12-06 19:22:11 -0500

    Ridge

--- Discriminative/ridgeResidual.m
+++ Discriminative/ridgeResidual.m
@@ -1,11 +1,13 @@
 function test_labels = ridgeResidual(train_inputs, train_labels, test_inputs)
     numoutputs = size(train_labels,2);
     test_labels = zeros(size(test_inputs,1),numoutputs);
-    [train_inputs_pre_r, test_inputs_pre_r] = preprocess(train_inputs, test_inputs, 110);
-    lambda = [10 10 10 5 0.0010 10 10 5 10];
+    
+    lambda = [50   100   100    50     1   100   100   100   100];
+    chosen_components = [75  75   150   125   250    50 60   75 50];
     D = size(train_inputs, 2);
     for i=1:numoutputs
         train_labels_i = train_labels(:,i);
+        [train_inputs_pre_r, test_inputs_pre_r] = preprocess(train_inputs, test_inputs, chosen_components(i));
         b = ridge(train_labels_i, train_inputs_pre_r, lambda(i),0);
         test_labels(:,i) = test_inputs_pre_r*b(2:end,:)+b(1,:);
         predicted_train_labels(:,i) = train_inputs_pre_r*b(2:end,:)+b(1,:);
